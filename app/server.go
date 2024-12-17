ce2cee4466eece1e73509da385857adb22b6bd2b
ecoarchie <eco.artur@gmail.com>
2024-12-17 16:34:08 +0300

    add: serial request

--- app/server.go
+++ app/server.go
@@ -32,34 +32,37 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	buff := bufio.NewReader(conn)
-	var messageSize int32
-	binary.Read(buff, binary.BigEndian, &messageSize)
-	fmt.Printf("message size is %d\n", messageSize)
+	for {
+		buff := bufio.NewReader(conn)
+		var messageSize int32
+		binary.Read(buff, binary.BigEndian, &messageSize)
+		fmt.Printf("message size is %d\n", messageSize)
 
-	var requestApiKey int16
-	binary.Read(buff, binary.BigEndian, &requestApiKey)
-	fmt.Printf("requestApiKey is %d\n", requestApiKey)
+		var requestApiKey int16
+		binary.Read(buff, binary.BigEndian, &requestApiKey)
+		fmt.Printf("requestApiKey is %d\n", requestApiKey)
 
-	var requestApiVersion int16
-	binary.Read(buff, binary.BigEndian, &requestApiVersion)
-	fmt.Printf("requestApiVersion is %d\n", requestApiVersion)
+		var requestApiVersion int16
+		binary.Read(buff, binary.BigEndian, &requestApiVersion)
+		fmt.Printf("requestApiVersion is %d\n", requestApiVersion)
 
-	var correlationID int32
-	binary.Read(buff, binary.BigEndian, &correlationID)
-	fmt.Printf("correlationID is %d\n", correlationID)
+		var correlationID int32
+		binary.Read(buff, binary.BigEndian, &correlationID)
+		fmt.Printf("correlationID is %d\n", correlationID)
 
-	keys := []api.ApiKey{}
-	apiKey := api.ApiKey{
-		Key:        requestApiKey,
-		MinVersion: 0,
-		MaxVersion: 4,
-		TagBuffer:  []byte{},
+		keys := []api.ApiKey{}
+		apiKey := api.ApiKey{
+			Key:        requestApiKey,
+			MinVersion: 0,
+			MaxVersion: 4,
+			TagBuffer:  []byte{},
+		}
+		keys = append(keys, apiKey)
+		response := api.NewApiKeyResponse(requestApiKey, requestApiVersion, correlationID, keys)
+
+		conn.Write(response.Encode())
+
 	}
-	keys = append(keys, apiKey)
-	response := api.NewApiKeyResponse(requestApiKey, requestApiVersion, correlationID, keys)
-
-	conn.Write(response.Encode())
 }
 
 type Header struct {
